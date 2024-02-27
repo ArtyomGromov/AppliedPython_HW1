2aaea9b9a981681ae326a8355975730541600eac
ArtyomGromov <83470880+ArtyomGromov@users.noreply.github.com>
2024-02-27 22:41:16 +0300

    Delete Procfile.py

--- Procfile.py
+++
@@ -1,5 +0,0 @@
-web: streamlit run distribution_plot.py
-web: streamlit run corr_matrix.py
-web: streamlit run target_dependence.py
-web: streamlit run stats_info.py
-web: streamlit run modelling.py
